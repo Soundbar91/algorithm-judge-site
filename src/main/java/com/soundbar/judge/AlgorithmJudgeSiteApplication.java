846e3ccd9e244542fd0ebf31906b0a07802a4489
신관규 <soundbar91@gmail.com>
2025-07-13 22:02:13 +0900

    project setting

---
+++ src/main/java/com/soundbar/judge/AlgorithmJudgeSiteApplication.java
@@ -0,0 +1,13 @@
+package com.soundbar.judge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmJudgeSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmJudgeSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soundbar/judge/AlgorithmJudgeSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soundbar.judge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmJudgeSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
